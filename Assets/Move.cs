c91833e88fef8bc4937765a1d263312893c940dd
JhonatandoMar <hashirojhon98@gmail.com>
2023-12-05 16:53:03 -0300

    Ajuste no script, Ajuste na cena e controle

---
+++ Assets/Move.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] bool isFacing;
+
+    Rigidbody2D rig;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    public void SetMove(InputAction.CallbackContext value)
+    {
+        moveDirection = value.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        MoveP();
+    }
+
+    private void MoveP()
+    {
+        rig.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+        if (moveDirection.x < 0 && isFacing == false)
+        {
+            Flip();
+        }
+        else if (moveDirection.x > 0 && isFacing == true)
+        {
+            Flip();
+        }
+    }
+
+    public void Flip()
+    {
+        isFacing = !isFacing;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
