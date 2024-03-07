698f4e9aafacb87ee2d60ff36268f4e73b9de49a
JhonatandoMar <72980049+JhonatandoMar@users.noreply.github.com>
2024-03-07 13:44:37 -0300

    Revert "Revert "Implementação de Dash e Ajuste no script""
    This reverts commit 60b95cd91640efedbed6f0a635977323efbd1186.

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -5,39 +5,55 @@ using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
-     
-    float speed = 7;
+    [Header("Walk Settings")]
+    float speed = 4.5f;
     bool isFacing,
          isWalking = false,
          CheckAttack;
 
-    Rigidbody2D rig;
+    public Rigidbody2D rig;
     public Vector2 moveDirection;
     public Animator playerAnimator;
 
     [Header("Dash Settigns")]
-    public float dashSpeed;
-    public float dashDuration;
-    public float dashCooldown;
+    float dashSpeed = 15f;
+    float dashDuration = 0.3f;
+    float dashCooldown = 0.5f;
     bool isDashing;
     bool canDash;
 
+    //Executa primeiro que Start
     private void Awake()
     {
         rig = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
+    //Executa apartir do Start
     private void Start()
     {
         isWalking = false;
+        isDashing = false;
+        canDash = true;
     }
 
+    //Input novo para movimento
     public void SetMove(InputAction.CallbackContext value)
 
     {
         moveDirection = value.ReadValue<Vector2>();
     }
 
+    //Input novo para Dash
+    public void SetDash(InputAction.CallbackContext value)
+    {
+        if (canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    //Input novo para Attack
     public void SetAttack(InputAction.CallbackContext value)
     {
         CheckAttack = true;
@@ -52,7 +68,11 @@ public class Move : MonoBehaviour
     {
         //rig.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
         //isWalking = (moveDirection.x != 0 || moveDirection.y != 0);
-        
+        if (isDashing)
+        {
+            return;
+        }
+
         if (CheckAttack)
         {
             rig.velocity = Vector2.zero;
@@ -96,6 +116,18 @@ public class Move : MonoBehaviour
        Vector3 theScale = transform.localScale;
        theScale.x *= -1;
        transform.localScale = theScale;
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rig.velocity = new Vector2(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 
 }
