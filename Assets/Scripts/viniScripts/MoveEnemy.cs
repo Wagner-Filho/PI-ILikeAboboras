d9a14d3250bf24cd2335490cc78e85726e1c108c
VinnieBM03 <vbmagalhaes003@gmail.com>
2024-03-12 17:57:44 -0300

    coisas

--- Assets/Scripts/viniScripts/MoveEnemy.cs
+++ Assets/Scripts/viniScripts/MoveEnemy.cs
@@ -11,15 +11,36 @@ public class MoveEnemy : MonoBehaviour
     [SerializeField] bool _dir;
     [SerializeField] bool _isFacing;
 
+    private Transform _target;
+    [SerializeField] float _distPlayer;
+
+    [SerializeField] float _distance;
+
+    [SerializeField] float _vdis;
     void Start()
     {
-
+        _target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     void Update()
     {
-        _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+
+
+        //        if (Vector2.Distance(_target.position, transform.position) < _distance)
+        //        {
+        //            _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+        //        }
+        _distPlayer = Vector2.Distance(_target.position, transform.position);
+
+        if (_distPlayer < _vdis)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _target.position, _speed*Time.deltaTime);
+        }
+        else
+        {
+            _rb.velocity = Vector2.zero;
+        }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall") && _dir == false)
         {
@@ -36,7 +57,6 @@ public class MoveEnemy : MonoBehaviour
         _dir = false;
 
     }
-
     public void Flip()
     {
         _isFacing = !_isFacing;
